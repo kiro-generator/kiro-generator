4a9e5d62f14bbead8d28d5b3fbc35ddd950a1657
dougeEfresh <dchimento@gmail.com>
2026-01-28 21:13:53 +0000

    feat: add Release config

--- .github/rust-ci.ts
+++ .github/rust-ci.ts
@@ -1,9 +1,10 @@
-import { createRustWorkflow } from '@dougefresh/ci';
+import { Arch, createRustWorkflow } from '@dougefresh/ci';
+
 const prompt = `
 
 ## kiro Documentation
 
-There are additional makrdown documents about kiro-cli configuration in directory ./docs/kiro
+There are additional markdown documents about kiro-cli configuration in directory ./docs/kiro
 If you need more context around kiro agent config refer to these files.
 
 
@@ -16,6 +17,13 @@ If unclear how the crate / library works, refer to these documents.
 `;
 export default function () {
   return createRustWorkflow()
+    .withRelease({
+      debian: true,
+      bin: true,
+      publish: false,
+      profile: 'release',
+      os: [Arch.AMD64],
+    })
     .semver(false)
     .additionalPrompt(prompt)
     .disableSanitizers()
