a31b34689fe9bd59806cad9a3d5e66b6d7e59425
dougeEfresh <dchimento@gmail.com>
2026-02-18 17:58:58 +0000

    feat: test release

--- .github/rust-ci.ts
+++ .github/rust-ci.ts
@@ -27,14 +27,17 @@ export default function () {
     .coverage(coverage)
     .withRelease({
       debian: true,
-      bin: true,
+      bin: {
+        name: 'kg',
+        linux: {
+          arm64: true,
+          amd64: true,
+        },
+        mac: true,
+        win: false,
+      },
       publish: false,
       profile: 'release',
-      os: [Os.LINUX_ARM64, Os.LINUX_AMD64, Os.MAC],
-      homebrew: {
-        if: true,
-        repo: 'homebrew-kiro-generator',
-      },
     })
     .semver(false)
     .disableSanitizers()
